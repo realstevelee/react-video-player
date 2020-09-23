93768950fb267923e96031721d9c09778bc1bc75
realstevelee <realstevelee@gmail.com>
2020-09-23 03:13:14 -0400

    added videolist and videoitem components

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
+import VideoList from './VideoList'
 
 export default class App extends Component {
+    state = { videos: [] }
 
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         })
+
+        // response.data.items is array
+        this.setState({ videos: response.data.items })
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                {/* Passing state as props*/}
+                <VideoList videos={this.state.videos} />
             </div>
         )
     }
